63a019d27821dffcadf3fe2f29e2062089bb7323
Perrard Alex <perrardalex@yahoo.com>
2019-10-19 16:15:22 +0200

    test 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-
-fn main() {
-
-}
+// extern crate clap;
+// use clap::App;
+//
+// fn main() {
+//     App::new("meng_app")
+//        .version("1.0")
+//        .about("clap_test")
+//        .author("Perrard Alex.")
+//        .get_matches();
+// }
+println!("HEllu")
